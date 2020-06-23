4463b0a0c747d90486e677573e7bf5a6e2b6aa53
josenz <josenz@qq.com>
2020-06-23 10:31:36 +0800

    model property

--- index.js
+++
@@ -1,2 +0,0 @@
-import "./src/app/example-city";
-
--- src/3dview/messageBox.js
+++
@@ -1,9 +0,0 @@
-export class MessageBox {
-    constructor() {
-        // 是否始终面向屏幕
-        this.oriention = true;
-        this.style = "";
-    }
-
-
-}
